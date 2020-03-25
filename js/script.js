bd8e62ce5801c72c2acc7cd53e0e1c53d3df80ac
Kanat Akmatov <kanat.96.akmatov@gmail.com>
2020-03-25 13:58:33 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$('.burger').on('click', ()=> {
+    $('.burger').toggleClass('show-menu');
+    $('.top-menu').toggleClass('show')
+});
+
+$('.top-menu a').on('click', ()=>{
+    $('.burger').removeClass('show-menu');
+    $('.top-menu') .removeClass('show')
+});
